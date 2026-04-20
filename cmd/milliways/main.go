0d6b8b4cb7000f40c5b014ad0c66239967d46246
Morgan Wigge <morgan@wigge.nu>
2026-04-20 12:01:18 +0200

    chore: bump version to v0.2.0

--- cmd/milliways/main.go
+++ cmd/milliways/main.go
@@ -34,7 +34,7 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var version = "0.1.0"
+var version = "0.2.0"
 
 // dispatchOpts groups the parameters for the dispatch function.
 type dispatchOpts struct {
