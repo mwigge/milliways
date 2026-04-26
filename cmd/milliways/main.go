238ef9cdc86959394605f4821bee714c2b812004
Morgan Wigge <morgan@wigge.nu>
2026-04-26 20:37:07 +0200

    feat: bump to v0.4.4

--- cmd/milliways/main.go
+++ cmd/milliways/main.go
@@ -34,7 +34,7 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var version = "0.4.3"
+var version = "0.4.4"
 
 // dispatchOpts groups the parameters for the dispatch function.
 type dispatchOpts struct {
